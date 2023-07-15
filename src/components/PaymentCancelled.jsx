c24a0668873dc637d47c0e4fac396d11b8f40615
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-07-15 19:18:16 +0100

    added real timer on PaymentCancelled and Successful

--- src/components/PaymentCancelled.jsx
+++ src/components/PaymentCancelled.jsx
@@ -1,21 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const PaymentCancelled = () => {
   const navigate = useNavigate();
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      navigate("/loggedHome");
-    }, 5000);
+  const [count, setCount] = useState(5);
 
-    return () => clearTimeout(timer);
-  }, [navigate]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(prevCount => prevCount - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (count === 0) {
+      navigate("/loggedHome");
+    }
+  }, [count, navigate]);
 
   return (
     <div className="text-center">
       <div>Your order has been cancelled</div>
-      <div>You will be redirected to the home page in 5 second...</div>
+      <div>You will be redirected to the home page in {count} seconds...</div>
     </div>
   );
 };
+
 export default PaymentCancelled;
--- src/components/PaymentSuccess.jsx
+++ src/components/PaymentSuccess.jsx
@@ -1,20 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const PaymentSuccess = () => {
   const navigate = useNavigate();
+  const [count, setCount] = useState(10);
+
   useEffect(() => {
-    setTimeout(() => {
+    const interval = setInterval(() => {
+      setCount(prevCount => prevCount - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (count === 0) {
       navigate("/loggedHome");
-    }, 8000);
-  }, [navigate]);
+    }
+  }, [count, navigate]);
 
   return (
     <div className="text-center">
       <div>Thank you for your purchase, your transaction has been accepted. Hope to see you again!</div>
       <div>We have sent you a confirmation email!</div>
-      <div>You will be redirected to the home page in 8 second...</div>
+      <div>You will be redirected to the home page in {count} seconds...</div>
     </div>
   );
 };
+
 export default PaymentSuccess;
