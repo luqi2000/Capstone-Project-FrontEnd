25237f47bc2728db456d99efb468f434fe8b60b2
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-07-15 14:19:37 +0100

    added <Modal> with messege from back-end for registration and added required options for login and registration

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -52,13 +52,13 @@ const Login = () => {
             <Form onSubmit={handleSubmit}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label className="text-white">Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" name="email" />
+                <Form.Control type="email" placeholder="Enter email" name="email" required />
                 <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label className="text-white">Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" name="password" />
+                <Form.Control type="password" placeholder="Password" name="password" required />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicCheckbox">
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 import MyNavbar from "./MyNavbar";
 import Myfooter from "./Myfooter";
 import { useState } from "react";
@@ -10,6 +10,9 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [address, setAddress] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
+  const [showErrorModal, setShowErrorModal] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -23,7 +26,6 @@ const Register = () => {
       phoneNumber
     };
 
-    //Send the data to the backend using Fetch
     fetch("http://localhost:3001/auth/register", {
       method: "POST",
       headers: {
@@ -31,19 +33,38 @@ const Register = () => {
       },
       body: JSON.stringify(userData)
     })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setName("");
-        setSurname("");
-        setEmail("");
-        setPassword("");
-        setAddress("");
-        setPhoneNumber("");
+      .then(response => {
+        if (response.status === 201) {
+          // Registration successful
+          setShowSuccessModal(true);
+          setName("");
+          setSurname("");
+          setEmail("");
+          setPassword("");
+          setAddress("");
+          setPhoneNumber("");
+        } else {
+          // Registration failed
+          response.json().then(data => {
+            setShowErrorModal(true);
+            setErrorMessage(data.message || "An error occurred during registration.");
+          });
+        }
       })
       .catch(error => {
         console.error(error);
+        setShowErrorModal(true);
+        setErrorMessage("An error occurred during registration.");
       });
+  };
+
+  const handleCloseSuccessModal = () => {
+    setShowSuccessModal(false);
+  };
+
+  const handleCloseErrorModal = () => {
+    setShowErrorModal(false);
+    setErrorMessage("");
   };
 
   return (
@@ -57,6 +78,7 @@ const Register = () => {
             placeholder="Please insert your name"
             value={name}
             onChange={e => setName(e.target.value)}
+            required
           />
         </Form.Group>
 
@@ -67,12 +89,19 @@ const Register = () => {
             placeholder="Please insert your surname"
             value={surname}
             onChange={e => setSurname(e.target.value)}
+            required
           />
         </Form.Group>
 
         <Form.Group className="mb-3 w-50" controlId="formBasicEmail">
           <Form.Label className="text-white">Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
           <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
         </Form.Group>
 
@@ -83,6 +112,7 @@ const Register = () => {
             placeholder="Please insert your password"
             value={password}
             onChange={e => setPassword(e.target.value)}
+            required
           />
           <Form.Text className="text-muted">We'll never share your password with anyone else.</Form.Text>
         </Form.Group>
@@ -94,6 +124,7 @@ const Register = () => {
             placeholder="Please insert your full address"
             value={address}
             onChange={e => setAddress(e.target.value)}
+            required
           />
         </Form.Group>
 
@@ -104,6 +135,7 @@ const Register = () => {
             placeholder="Please insert your Phone number"
             value={phoneNumber}
             onChange={e => setPhoneNumber(e.target.value)}
+            required
           />
         </Form.Group>
 
@@ -115,6 +147,34 @@ const Register = () => {
           Submit
         </Button>
       </Form>
+
+      <Modal show={showSuccessModal} onHide={handleCloseSuccessModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Registration Successful</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Please go to the Login page.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleCloseSuccessModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showErrorModal} onHide={handleCloseErrorModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Registration Error</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>{errorMessage}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleCloseErrorModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <Myfooter />
     </>
   );
